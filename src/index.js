5caae399311b6822a8352058aa3640aa1d3f27df
Talai Sergei <tsa@tut.by>
2020-10-01 23:41:29 +0300

    feat: add the solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function toReadable (number) {
-  
+    if (typeof number != 'number') number = parseInt(number)
+    const digits = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    const dozens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    if (number == 0) return 'zero'
+    else if (number < 20) {
+        return digits[number]
+    } else if (number < 100) {
+        const $_num = Math.trunc(number / 10)
+        const $_digit = digits[number - $_num * 10]
+        return dozens[$_num] + ($_digit.length != 0 ? ' ' + $_digit : '')
+    } else if (number < 1000) {
+        const $_num = Math.trunc(number / 100)
+        const $_digits = toReadable(number - $_num * 100)
+        return digits[$_num] + ' hundred' + ($_digits != 'zero' ? ' ' + $_digits : '')
+    }
 }
